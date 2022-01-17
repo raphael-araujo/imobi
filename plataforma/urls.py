29f22c46c0b0231ce9befc4a4fa3bd13ab0d1a3c
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-01-17 00:01:13 -0300

    imobi

---
+++ plataforma/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('imovel/<str:id>', views.imovel, name='imovel'),
+    path('agendar_visita', views.agendar_visita, name='agendar_visita'),
+    path('agendamentos', views.agendamentos, name='agendamentos'),
+    path('cancelar_agendamento/<str:id>', views.cancelar_agendamento, name='cancelar_agendamento')
+]
---
+++ plataforma/views.py
@@ -0,0 +1,69 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Imovel, Cidade, Visitas
+
+
+# Create your views here.
+
+
+@login_required(login_url='/auth/logar')
+def home(request):
+    cidades = Cidade.objects.all()
+    preco_minimo = request.GET.get('preco_minimo')
+    preco_maximo = request.GET.get('preco_maximo')
+    cidade = request.GET.get('cidade')
+    tipo = request.GET.getlist('tipo')
+    if preco_minimo or preco_maximo or cidade or tipo:
+        if not preco_minimo:
+            preco_minimo = 0
+        if not preco_maximo:
+            preco_maximo = 9999999999
+        if not tipo:
+            tipo = ['A', 'C']
+
+        imoveis = Imovel.objects.filter(valor__gte=preco_minimo) \
+            .filter(valor__lte=preco_maximo) \
+            .filter(tipo_imovel__in=tipo).filter(cidade=cidade)
+    else:
+        imoveis = Imovel.objects.all()
+        
+    return render(request, 'home.html', {'imoveis': imoveis, 'cidades': cidades})
+
+
+def imovel(request, id):
+    imovel = get_object_or_404(Imovel, id=id)
+    sugestoes = Imovel.objects.filter(cidade=imovel.cidade).exclude(id=id)[:2]
+    print(imovel)
+    return render(request, 'imovel.html', {'imovel': imovel, 'sugestoes': sugestoes})
+
+
+def agendar_visita(request):
+    usuario = request.user
+    dia = request.POST.get('dia')
+    horario = request.POST.get('horario')
+    id_imovel = request.POST.get('id_imovel')
+    
+    visitas = Visitas(
+        imovel_id=id_imovel,
+        usuario=usuario,
+        dia=dia,
+        horario=horario
+    )
+    visitas.save()
+    
+    return redirect('/agendamentos')
+    
+    
+def agendamentos(request):
+    visitas = Visitas.objects.filter(usuario=request.user)
+    return render(request, 'agendamentos.html', {'visitas': visitas})
+
+
+def cancelar_agendamento(request, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = 'C'
+    visitas.save()
+    return redirect('/agendamentos')
+
+
